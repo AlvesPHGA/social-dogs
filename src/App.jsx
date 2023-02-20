7fdce19fc45b5896e89b8f373aed29914bc1b865
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-20 15:00:57 -0300

    updated app doc

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 function App() {
    return (
@@ -10,6 +11,7 @@ function App() {
             <Header />
             <Routes>
                <Route path="/" element={<Home />} />
+               <Route path="/login" element={<Login />} />
             </Routes>
             <Footer />
          </BrowserRouter>
