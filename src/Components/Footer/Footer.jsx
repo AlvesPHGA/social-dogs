ba1d46012738eccb864c20eb29daac215129eb39
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-20 14:45:17 -0300

    created tow doc to Header, with name Footer and FooterStyle

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FooterStyle } from './Footer.style';
+
+const Footer = () => {
+   return (
+      <FooterStyle>
+         <h1>Footer</h1>
+      </FooterStyle>
+   );
+};
+
+export default Footer;
