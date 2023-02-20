17caabda58dbc729b769080e00fe95f9253f93fd
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-20 15:45:00 -0300

    updated GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 
--- src/Styles/GlobalStyle.style.jsx
+++ src/Styles/GlobalStyle.style.jsx
@@ -7,11 +7,37 @@ export const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
    }
 
+   body{
+      --type-first: Helvetica, Arial, sans-serif;
+      --type-second: 'Spectral', Georgia;
+
+      color: #333;
+      font-family: var(--type-first);
+   }
+
+   h1, h2, h3, h4, p{
+      margin: 0;
+   }
+
+   ul, li{
+      list-style: none;
+      padding: 0;
+      margin: 0;
+   }
+
    a{
       text-decoration: none;
    }
 
    img{
       max-width: 100%;
+      display: block;
+   }
+
+   button, input{
+      display: block;
+      font-size: 1rem;
+      font-family: var(--type-first);
+      color: #333;
    }
 `;
