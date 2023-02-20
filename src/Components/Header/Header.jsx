5ab4f6ed97d004a6fa3059bb677a3e833b7f8a34
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-20 16:19:38 -0300

    updated vite config doc and Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,12 +3,16 @@ import { HeaderStyle } from './Header.style';
 import { Link } from 'react-router-dom';
 import { Container } from '../../Styles/Styles.style';
 
+import { ReactComponent as Dogs } from '../../assets/dogs.svg';
+
 const Header = () => {
    return (
       <HeaderStyle>
          <Container>
             <nav>
-               <Link to="/">Home</Link>
+               <Link to="/">
+                  <Dogs />
+               </Link>
                <Link to="/login">Login / Criar</Link>
             </nav>
          </Container>
