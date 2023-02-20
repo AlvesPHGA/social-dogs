e56e485f7c48b96e9b43a46ffe51a0ebac8ba7ca
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-20 14:42:49 -0300

    created tow doc to Header, with name Header and HeardeStyle

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeaderStyle } from './Header.style';
+
+const Header = () => {
+   return (
+      <HeaderStyle>
+         <h1>Header</h1>
+      </HeaderStyle>
+   );
+};
+
+export default Header;
