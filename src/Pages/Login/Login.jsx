a50e5913192ebc18ee2df78c43b8249bb790310f
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-22 10:37:26 -0300

    created docs and routers to login create, form, lost pass and reset

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router';
+import { Routes } from 'react-router-dom';
+import LoginCreate from './LoginCreate/LoginCreate';
+import LoginForm from './LoginForm/LoginForm';
+import LoginLost from './LoginLost/LoginLost';
+import LoginReset from './LoginReset/LoginReset';
 
 const Login = () => {
-   return <div>Login</div>;
+   return (
+      <>
+         <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="create" element={<LoginCreate />} />
+            <Route path="passwordlost" element={<LoginLost />} />
+            <Route past="reset" element={<LoginReset />} />
+         </Routes>
+      </>
+   );
 };
 
 export default Login;
