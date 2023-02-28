4306c583f9f0655192db55d6a4603effbde73a28
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-28 11:53:47 -0300

    updated api doc, was add a new function with name USER_POST

--- src/api.jsx
+++ src/api.jsx
@@ -40,3 +40,19 @@ export const USER_GET = (token) => {
       },
    };
 };
+
+// Create new user
+export const USER_POST = (body) => {
+   return {
+      url: API_URL + '/api/user',
+
+      options: {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+
+         body: JSON.stringify(body),
+      },
+   };
+};
