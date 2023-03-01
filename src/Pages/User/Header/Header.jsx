603fbfc296bf34c4a802144c2ca84df12f314b45
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-01 11:38:44 -0300

    created a style doc to Header in User page

--- src/Pages/User/Header/Header.jsx
+++ src/Pages/User/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Title } from '../../../Components/Global/Title';
+import Nav from '../Nav/Nav';
+import { UserHeader } from './Header.style';
 
 const Header = () => {
-   return <div>Header</div>;
+   return (
+      <UserHeader>
+         <Title>Title</Title>
+         <Nav></Nav>
+      </UserHeader>
+   );
 };
 
 export default Header;
