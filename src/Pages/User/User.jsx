0b856d66170839c6a323ca936270f77bc1043378
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-01 11:30:53 -0300

    add Routes in User.jsx

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from '../../Styles/Styles.style';
+import Feeds from '../Feeds/Feeds';
+import Header from './Header/Header';
+import PhotoPost from './PhotoPost/PhotoPost';
+import Stats from './Stats/Stats';
 
 const User = () => {
-   return <div>User</div>;
+   return (
+      <Container>
+         <Header />
+         <Routes>
+            <Route path="/" element={<Feeds />} />
+            <Route path="post" element={<PhotoPost />} />
+            <Route path="stats" element={<Stats />} />
+         </Routes>
+      </Container>
+   );
 };
 
 export default User;
