7fc600fcfa49edff8310f4d8c563d0a98dd615e1
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-02 11:33:23 -0300

    updated userHeader layout

--- src/Pages/User/Header/Header.jsx
+++ src/Pages/User/Header/Header.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Title } from '../../../Components/Global/Title';
 import Nav from '../Nav/Nav';
 import { UserHeader } from './Header.style';
 
 const Header = () => {
+   const [title, setTitle] = React.useState('');
+   const local = useLocation();
+
+   React.useEffect(() => {
+      const { pathname } = local;
+
+      switch (pathname) {
+         case '/accout/stats':
+            setTitle('Estatisticas');
+            break;
+
+         case '/accout/post':
+            setTitle('Adicionar novo');
+            break;
+
+         default:
+            setTitle('Minha conta');
+      }
+   }, [local]);
+
    return (
       <UserHeader>
-         <Title>Title</Title>
+         <Title>{title}</Title>
          <Nav></Nav>
       </UserHeader>
    );
