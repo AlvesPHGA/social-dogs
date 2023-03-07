52194db97957d782d66779ba5ffc2e681f5777fc
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-07 10:28:43 -0300

    updated FeedPhotoItems and created a style doc to FeedPhotoItems

--- src/Pages/Feeds/FeedPhotoItems/FeedPhotoItems.jsx
+++ src/Pages/Feeds/FeedPhotoItems/FeedPhotoItems.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { PhotoItem } from './FeedPhotoItems.style';
 
-const FeedPhotoItems = () => {
-   return <div>FeedPhotoItems</div>;
+const FeedPhotoItems = ({ photo }) => {
+   return (
+      <PhotoItem>
+         <img src={photo.src} alt={photo.title} />
+         <span>{photo.acesso}</span>
+      </PhotoItem>
+   );
 };
 
 export default FeedPhotoItems;
