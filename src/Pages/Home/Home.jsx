2eaf8c186c15e628151b8a0e42adc0bb78cedb67
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-07 09:56:25 -0300

    created a new style doc to Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Container } from '../../Styles/Styles.style';
+import Feeds from '../Feeds/Feeds';
+import { HomeStyle } from './Home.style';
 
 const Home = () => {
-   return <div>Home</div>;
+   return (
+      <HomeStyle>
+         <Container>
+            <Feeds />
+         </Container>
+      </HomeStyle>
+   );
 };
 
 export default Home;
