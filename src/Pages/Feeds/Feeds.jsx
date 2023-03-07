0869a2ad0a38e9574e37336303b0415a379c194c
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-07 10:00:10 -0300

    created a new doc to Photos

--- src/Pages/Feeds/Feeds.jsx
+++ src/Pages/Feeds/Feeds.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from '../../Styles/Styles.style';
 import FeedModal from './FeedModal/FeedModal';
+import FeedPhotos from './FeedPhotos/FeedPhotos';
 
 const Feeds = () => {
    return (
       <Container>
          <FeedModal />
+         <FeedPhotos />
       </Container>
    );
 };
