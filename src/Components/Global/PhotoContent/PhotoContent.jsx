dcf98e0293a67a3066ec8cdcae83c2a142c144bf
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-08 11:38:51 -0300

    updated some docs

--- src/Components/Global/PhotoContent/PhotoContent.jsx
+++ src/Components/Global/PhotoContent/PhotoContent.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
-import { PhotoContentStyle } from './PhotoContent.style';
+import { Link } from 'react-router-dom';
+import PhotoComments from '../PhotoComments/PhotoComments';
+import { Title } from '../Title';
+import {
+   BoxAttributeDetails,
+   BoxContentDetails,
+   BoxDetails,
+   BoxImage,
+   PhotoContentStyle,
+} from './PhotoContent.style';
 
 const PhotoContent = ({ data }) => {
-   return <PhotoContentStyle></PhotoContentStyle>;
+   const { photo, comments } = data;
+
+   console.log(photo);
+
+   return (
+      <PhotoContentStyle>
+         <BoxImage>
+            <img src={photo.src} alt={photo.title} />
+         </BoxImage>
+         <BoxDetails>
+            <BoxContentDetails>
+               <p>
+                  <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+                  <span className="views">{photo.acessos}</span>
+               </p>
+
+               <Title>
+                  <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+               </Title>
+
+               <BoxAttributeDetails>
+                  <li>{photo.peso} Kg</li>
+                  <li>{photo.idade} anos</li>
+               </BoxAttributeDetails>
+            </BoxContentDetails>
+         </BoxDetails>
+         <PhotoComments id={photo.id} comments={comments} />
+      </PhotoContentStyle>
+   );
 };
 
 export default PhotoContent;
--- src/Components/Global/PhotoContent/PhotoContent.style.jsx
+++ src/Components/Global/PhotoContent/PhotoContent.style.jsx
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
 export const PhotoContentStyle = styled.div``;
+
+export const BoxImage = styled.div``;
+
+export const BoxDetails = styled.div``;
+
+export const BoxContentDetails = styled.div``;
+
+export const BoxAttributeDetails = styled.ul``;
