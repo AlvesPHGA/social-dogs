5845f2779db25b5d1f25b56ee1359d3e85db0aa3
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-09 09:55:45 -0300

    updated style PhotoContent

--- src/Components/Global/PhotoContent/PhotoContent.style.jsx
+++ src/Components/Global/PhotoContent/PhotoContent.style.jsx
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 
-export const PhotoContentStyle = styled.div``;
+export const PhotoContentStyle = styled.div`
+   margin: auto;
+   height: 36rem;
+   border-radius: 0.2rem;
+   background: white;
+   display: grid;
+   grid-template-columns: 36rem 20rem;
+   grid-template-rows: auto 1fr auto;
+   overflow: hidden;
+`;
 
-export const BoxImage = styled.div``;
+export const BoxImage = styled.div`
+   grid-row: 1/4;
+`;
 
-export const BoxDetails = styled.div``;
+export const BoxDetails = styled.div`
+   padding: 2rem 2rem 0 2rem;
+`;
 
 export const BoxContentDetails = styled.div``;
 
