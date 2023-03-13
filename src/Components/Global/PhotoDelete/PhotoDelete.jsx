68f4b9fe1fdde8310b236fda5028ee25a0c998cb
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-13 15:41:47 -0300

    updated delete

--- src/Components/Global/PhotoDelete/PhotoDelete.jsx
+++ src/Components/Global/PhotoDelete/PhotoDelete.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { DELETE_PHOTO } from '../../../api';
+import useFetch from '../../../Hooks/useFetch';
 import { ButtonDelete } from './PhotoDelete.style';
 
 const PhotoDelete = ({ id }) => {
+   const { load, request } = useFetch();
+
+   async function handleDeletePhoto() {
+      const alert = window.confirm('Tem certeza que deseja deletar?');
+
+      if (alert) {
+         const { url, options } = DELETE_PHOTO(id);
+         const { res } = await request(url, options);
+         if (res.ok) window.location.reload();
+      }
+   }
    return (
       <>
-         <ButtonDelete>Deletar</ButtonDelete>
+         {load ? (
+            <ButtonDelete disabled>Deletar</ButtonDelete>
+         ) : (
+            <ButtonDelete onClick={handleDeletePhoto}>Deletar</ButtonDelete>
+         )}
       </>
    );
 };
