ad297a0e64a19582bed7ebd247c4d9150ce27b83
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-13 15:47:23 -0300

    updated style component PhotoDelete

--- src/Components/Global/PhotoDelete/PhotoDelete.style.jsx
+++ src/Components/Global/PhotoDelete/PhotoDelete.style.jsx
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const ButtonDelete = styled.button``;
+export const ButtonDelete = styled.button`
+   background: #ddd;
+   border-radius: 0.4rem;
+   border: 1px solid transparent;
+   cursor: pointer;
+   font-family: var(--type-first);
+   font-size: 0.875rem;
+   line-height: 1;
+   padding: 0.3rem 0.6rem;
+   transition: 0.1s;
+
+   &:focus,
+   &:hover {
+      outline: none;
+      background: white;
+      box-shadow: 0 0 0 3px #eee;
+      border-color: #333;
+   }
+`;
