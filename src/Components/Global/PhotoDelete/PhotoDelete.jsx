ff453ce72d0e3d180e3e44372161ff4cc6763263
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-13 15:03:19 -0300

    created a new component with name PhotoDelele and this a style doc to component

---
+++ src/Components/Global/PhotoDelete/PhotoDelete.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ButtonDelete } from './PhotoDelete.style';
+
+const PhotoDelete = ({ id }) => {
+   return (
+      <>
+         <ButtonDelete>Deletar</ButtonDelete>
+      </>
+   );
+};
+
+export default PhotoDelete;
---
+++ src/Components/Global/PhotoDelete/PhotoDelete.style.jsx
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const ButtonDelete = styled.button``;
