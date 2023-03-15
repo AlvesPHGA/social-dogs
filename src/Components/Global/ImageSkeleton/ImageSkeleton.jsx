64297751115f2d3b394c0d2f728227e59edcfa42
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-15 12:02:33 -0300

    updated some docs, ImageSkeleton and style ImageSkeleton, was add a function to load image

--- src/Components/Global/ImageSkeleton/ImageSkeleton.jsx
+++ src/Components/Global/ImageSkeleton/ImageSkeleton.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { ImageSkeletonStyle } from './ImageSkeleton.style';
 
 const ImageSkeleton = ({ alt, ...props }) => {
+   const [skeleton, setSkeleton] = React.useState(true);
+   function handleLoadImage({ target }) {
+      setSkeleton(false);
+      target.style.opacity = 1;
+   }
    return (
       <ImageSkeletonStyle>
-         <div className="skeleton"></div>
-         <img className="image" alt={alt} {...props} />
+         {skeleton && <div className="skeleton"></div>}
+         <img onLoad={handleLoadImage} className="image" alt={alt} {...props} />
       </ImageSkeletonStyle>
    );
 };
