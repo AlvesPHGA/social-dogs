2b53133879dcc74cf1d874efedef976676affd4d
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-21 12:11:51 -0300

    updated loginReset

--- src/Pages/Login/LoginReset/LoginReset.jsx
+++ src/Pages/Login/LoginReset/LoginReset.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Button from '../../../Components/Form/Button/Button';
+import Input from '../../../Components/Form/Input/Input';
+import { Title } from '../../../Components/Global/Title';
+import useForm from '../../../Hooks/useForm';
 
 const LoginReset = () => {
-   return <div>LoginReset</div>;
+   const [login, setLogin] = React.useState('');
+   const [key, setKey] = React.useState('');
+
+   return (
+      <section>
+         <Title>Resetar senha</Title>
+         <form>
+            <Input
+               legend="Digite uma nova senha"
+               type="password"
+               userName="passReset"
+               {...pass}
+            />
+            <Button>Resetar</Button>
+         </form>
+      </section>
+   );
 };
 
 export default LoginReset;
