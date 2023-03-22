89a9540d71db736b7148f32f5e4cd355dce2acea
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-03-22 18:26:52 -0300

    updated starsGraph

--- src/Pages/User/Stats/StatsGraphs/StatsGraph.jsx
+++ src/Pages/User/Stats/StatsGraphs/StatsGraph.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import { AnimaLeft } from '../../../../Styles/Styles.style';
 import { StatsGraphStyle } from './StatsGraph.style';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const StatsGraph = ({ data }) => {
    const [graph, setGraph] = React.useState([]);
    const [total, setTotal] = React.useState(0);
 
    React.useEffect(() => {
-      const access = data
-         .map(({ acessos }) => Number(acessos))
-         .reduce((a, b) => a + b);
-      setTotal(access);
+      const graphData = data.map((item) => {
+         return {
+            x: item.title,
+            y: +item.acessos,
+         };
+      });
+      // const access = data
+      //    .map(({ acessos }) => Number(acessos))
+      //    .reduce((a, b) => a + b);
+      // setTotal(access);
+      console.log(data);
+      // setGraph(graphData);
    }, [data]);
 
    return (
       <StatsGraphStyle>
          <AnimaLeft>
-            <div className="total">
+            <div className="total graphItem">
                <p>Acessos: {total}</p>
+            </div>
+            <div className="graphItem">
+               <VictoryPie
+                  data={graph}
+                  innerRadius={50}
+                  padding={{
+                     top: 20,
+                     bottom: 20,
+                     left: 80,
+                     right: 80,
+                  }}
+                  style={{
+                     data: {
+                        fillOpacity: 0.9,
+                        stroke: '#fff',
+                        strokeWidth: 2,
+                     },
+
+                     labels: {
+                        fontSize: 14,
+                        fill: '#333',
+                     },
+                  }}
+               />
             </div>
          </AnimaLeft>
       </StatsGraphStyle>
--- src/Pages/User/Stats/StatsGraphs/StatsGraph.style.jsx
+++ src/Pages/User/Stats/StatsGraphs/StatsGraph.style.jsx
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const StatsGraphStyle = styled.section``;
+export const StatsGraphStyle = styled.section`
+   display: grid;
+   grid-template-columns: repeat(2, 1fr);
+   gap: 2rem;
+
+   & .total {
+      grid-column: 1/3;
+      padding: 2rem;
+      font-size: 2rem;
+   }
+
+   & .graphItem {
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
+      border-radius: 0.2rem;
+      display: grid;
+      align-items: center;
+   }
+`;
